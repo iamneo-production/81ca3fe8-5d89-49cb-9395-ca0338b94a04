4a2ce678fbb6fa42af5c08a69cd99bf8ae4ebbd7
DurgaPrasadYerupalli <dp868352@gmail.com>
2023-08-09 20:14:43 +0530

    Title change

--- demo/src/App.js
+++ demo/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>DEMO APPLICATION</h1>
+      <h1>Applications</h1>
       <h1>for testing purpose</h1>
     </div>
   );
